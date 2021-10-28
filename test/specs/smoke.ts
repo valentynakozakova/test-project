329af025ee881962b26cb84d1654c0a78f2409b9
Valentyna Kozakova <valentyna.kozakova@heathmont.net>
2021-10-28 14:22:38 +0300

    Add the project

--- test/specs/smoke.ts
+++ test/specs/smoke.ts
@@ -4,4 +4,12 @@ describe('Website', function () {
         expect($('.header-logo-img')).toBeDisabled();
         browser.pause(10000)
     })
+
+    it('should allow user to register', function () {
+        browser.url('/');
+        $('span.userbar__button-text').click();
+        const content = $('.popup-block login');
+        
+    })
 })
+
